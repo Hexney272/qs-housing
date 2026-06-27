0e85522dce27628944c65f97c5aac5142068f498
VeZse <141773707+Hexney272@users.noreply.github.com>
2026-06-27 16:36:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,100 @@
+
+
+
+
+
+
+--[[
+	These custom shells are a collaboration between Quasar Store and SubhaMPro.
+	Designed by SubhaMPro (https://subhampro.tebex.io/), integrated and distributed by Quasar Store.
+]]
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+version '5.3.38'
+
+name 'qs-housing'
+author 'Quasar Store'
+
+ui_page 'web/build/index.html'
+-- ui_page 'http://localhost:3005/' -- dev
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'shared/*.lua',
+
+	'config/main.lua',
+	'config/furniture.lua',
+	'config/assistant.lua',
+
+	'locales/locale.lua',
+}
+
+client_scripts {
+	'client/custom/**',
+	'client/*.lua',
+	'client/modules/**'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+
+	'config/webhooks.lua',
+	'config/fivemanage.lua',
+
+	'server/custom/**/**.lua',
+
+	'server/modules/db/main.lua',
+	'server/modules/db/creator.lua',
+	'server/modules/db/decorate.lua',
+	'server/modules/db/furnitureOrder.lua',
+	'server/modules/db/furniture.lua',
+	'server/modules/db/furnitureShop.lua',
+	'server/modules/db/metakey.lua',
+
+	'server/*.lua',
+	'server/modules/*.lua'
+}
+
+ox_libs {
+	'table',
+	'math'
+}
+
+files {
+	'web/build/**',
+	'web/images/**',
+	'web/sounds/**',
+
+	'locales/**'
+}
+
+dependencies {
+	'ox_lib',
+	'bob74_ipl',
+	'screenshot-basic',
+	'/gameBuild:3095'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/qs_gradient_00.ytyp'
+
+escrow_ignore {
+	'config/*.lua',
+	'client/custom/**',
+	'server/custom/**',
+	'server/webhooks',
+	'server/modules/metakey.lua',
+	'server/modules/smartphone_battery_bridge.lua',
+}
+
+dependency '/assetpacks'
+
+
+
+
+
+
